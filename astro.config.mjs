9badba96667445fa55d51aab500d9fe5a7241de4
anto0102 <antony.flex01@gmail.com>
2025-03-02 16:36:46 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,9 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Prova',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				discord: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
 				{
